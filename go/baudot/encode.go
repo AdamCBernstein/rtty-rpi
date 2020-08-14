1fe9c10e2ba93ec2e3cc95f4a1756b0b37e723c5
Adam Bernstein <adam.bernstein@pobox.com>
2020-08-13 18:20:29 -0700

    Less confusing slice allocation
    Cleaned up asciiToBaudot() slice allocation for readability.
    Added punctuation test.

--- go/baudot/encode.go
+++ go/baudot/encode.go
@@ -2,25 +2,14 @@ package baudot
 
 type baudotBits [8]bool
 
-func (c *Convert) shiftLettersFigures(retBits []baudotBits, shift bool) []baudotBits {
-	if c.shift == shift {
-		return nil
-	}
-
-	c.shift = shift
-	if shift {
-		return append(retBits, baudotConv[shiftUp])
-	} else {
-		return append(retBits, baudotConv[shiftDown])
-	}
-}
-
 func (c *Convert) asciiToBaudot(r rune) ([]baudotBits, bool) {
+	retBits := make([]baudotBits, 0, 3)
+
 	// Deal with control characters first
 	switch r {
 	case carriageReturn, lineFeed:
 		// Force downshift on CR/LF to keep Teletype shift state in-sync
-		return append(make([]baudotBits, 0, 3),
+		return append(retBits,
 			baudotConv[carriageReturn],
 			baudotConv[lineFeed],
 			baudotConv[shiftDown]), true
@@ -32,6 +21,23 @@ func (c *Convert) asciiToBaudot(r rune) ([]baudotBits, bool) {
 	if bits, ok := baudotConv[r]; !ok {
 		return nil, false
 	} else {
-		return append(c.shiftLettersFigures(make([]baudotBits, 0, 2), bits[LTRS_FIGS_BIT]), bits), true
+		if bitsLtrsFigs, ok := c.shiftLettersFigures(retBits, bits[LTRS_FIGS_BIT]); !ok {
+			return []baudotBits{bits}, true
+		} else {
+			return append(bitsLtrsFigs, bits), true
+		}
+	}
+}
+
+func (c *Convert) shiftLettersFigures(retBits []baudotBits, shift bool) ([]baudotBits, bool) {
+	if c.shift == shift {
+		return nil, false
+	}
+
+	c.shift = shift
+	if shift {
+		return append(retBits, baudotConv[shiftUp]), true
+	} else {
+		return append(retBits, baudotConv[shiftDown]), true
 	}
 }
--- go/main.go
+++ go/main.go
@@ -21,14 +21,18 @@ func main() {
 }
 
 func printTest(c *baudot.Convert) {
-	spaces := "SpaceTest:B         1         2         3         4         5    E"
-	I0 := "Z0Y1X2W3V4U5T6S7R8P9Q0Z0Y1X2W3V4U5T6S7R8P9Q0Z0Y1X2W3V4U5T6S7R8P9Q0"
-	test := "the quick brown fox jumped over the lazy dog's back     1234567890\n" +
-		"ryryryryryryryryryryryryryryryryryryryryryryryryryryryryryryryryry\n" +
-		"sgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsg"
+	const (
+		spaces   = "SpaceTest:B         1         2         3         4         5    E"
+		puncts   = "Punctuation: !@#$%^&*()_+={}[]:;\"',.?/  !@#$%^&*()_+={}[]:;\"',.?/  !@#$%^&*()_+={}[]:;\"',.?/ "
+		figsLtrs = "Z0Y1X2W3V4U5T6S7R8P9Q0Z0Y1X2W3V4U5T6S7R8P9Q0Z0Y1X2W3V4U5T6S7R8P9Q0"
+		test     = "the quick brown fox jumped over the lazy dog's back     1234567890\n" +
+			"ryryryryryryryryryryryryryryryryryryryryryryryryryryryryryryryryry\n" +
+			"sgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsgsg"
+	)
 
 	c.PrintLine(spaces)
-	c.PrintLine(I0)
+	c.PrintLine(puncts)
+	c.PrintLine(figsLtrs)
 	c.PrintLine("")
 	c.PrintLine(test)
 	c.PrintLine(test)
