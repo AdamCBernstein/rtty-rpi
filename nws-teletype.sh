2a5c48b2366ae51ac122d373594ad5d1d8cfa309
Adam Bernstein <adam.bernstein@pobox.com>
2021-02-15 11:52:39 -0800

    Add NWS crontab script
    Read nws-teletype.sh for how to use this script.
    Depends on this project https://github.com/AdamCBernstein/nws-http.

---
+++ nws-teletype.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+export PATH=$PATH:/usr/local/bin
+LOG="/tmp/nws-teletype.log"
+
+# Download local area weather forecast, and print on Model 28 Teletype
+# This is intented to be run as a cron job.
+# Example crontab entry
+# */20 * * * * /usr/local/bin/nws-teletype.sh
+# This is script is dependent this project
+#    https://github.com/AdamCBernstein/nws-http
+
+file1=/var/tmp/nws/sew.txt
+file2=/var/tmp/nws/sew2.txt
+file_print=$file1
+print=0
+
+if [ ! -f "$file1" ]; then
+  file_print="$file1"
+  nws-http sew > "$file_print"
+  print=1
+else
+  file_print="$file2"
+  nws-http sew > "$file_print"
+  cmp "$file1" "$file2"
+  if [ $? -ne 0 ]; then
+    print=1
+    cp "$file2" "$file1"
+  fi
+fi
+
+if [ $print -eq 1 ]; then
+  echo "$(date) rtty $file_print" >> "$LOG"
+  nice 0 rtty "$file_print" > /dev/null 2>&1
+else
+  echo "$(date) '$file_print' has not changed" >> "$LOG"
+fi
